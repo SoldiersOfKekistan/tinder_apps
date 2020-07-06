c734c60d12200348a07e989e1ff9329f8941c391
unknown <ifj.torok.attila@gmail.com>
2020-07-06 12:03:13 +0200

    Fixed merging error and wrong filenames

--- wc.py
+++ wc.py
@@ -5,6 +5,10 @@ Uses merged bio files, so if you don't have them already, run bio_merger.py.
 
 from path import Path
 from helper_functions import extract_data, lemmatize_sentence
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk import word_tokenize, pos_tag
+from nltk.corpus import wordnet
 
 def get_words(file):
     words = {}
@@ -12,10 +16,11 @@ def get_words(file):
 
     try:
         with open(file, encoding='utf-8', errors='replace') as bios_file:
+            n_profiles = 0
             profile = ""
             for line in bios_file:
                 if line == ";\n":
-                    bio, age, n_pictures = self.extract_data(profile)
+                    bio, age, n_pictures = extract_data(profile)
                     lemmatized = lemmatize_sentence(bio, lemmatizer)
                     for word in lemmatized:
                         if word in words:
@@ -23,6 +28,8 @@ def get_words(file):
                         else:
                             words[word] = 1
                     profile = ""
+                    n_profiles += 1
+                    print(str(n_profiles)+" profiles extracted", end="\r")
                 else:
                     profile += line
         return words
@@ -36,7 +43,7 @@ def write_list(file, _list):
             out_file.write(i[0]+":"+str(i[1])+"\n")
         
 def merge_dicts(d1, d2):
-    out = {key: value[:] for key, value in d1.items()}
+    out = {key: value for key, value in d1.items()}
     for k in d2.keys():
         if k in out:
             out[k] += d2[k]
@@ -59,6 +66,6 @@ men_list = sort_dict(men)
 women_list = sort_dict(women)
 merged_list = sort_dict(merged)
 
-write_list(Path().out+"/men_words", men_list)
-write_list(Path().out+"/women_words", women_list)
-write_list(Path().out+"/merged_words", merged_list)
+write_list(Path().out+"/men_words.txt", men_list)
+write_list(Path().out+"/women_words.txt", women_list)
+write_list(Path().out+"/merged_words.txt", merged_list)
